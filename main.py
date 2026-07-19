305fb1d7e0ac4e3bed95947e96dfcb4626871044
xMASAKIx <110505678+xMASAKIx@users.noreply.github.com>
2026-07-19 20:20:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,18 @@ MONITOR_TARGETS = [
         "parts": ["HAIR"]  # 監控這人的髮型
     },
     {
+        "name": "台灣石油王",
+        "ppsn": "20372100005972917",
+        "webhook_url": "https://discord.com/api/webhooks/1497581197201506376/PlEHn82RIwE1OVEtuvO1n317L0b0i91l2b8uwGs0mPE9-izVcqkJ49Uxd6rlV_30NzDq",
+        "parts": ["HAIR"]  # 監控這人的髮型
+    },
+    {
+        "name": "韓國石油王",
+        "ppsn": "20372100000900216",
+        "webhook_url": "https://discord.com/api/webhooks/1497581197201506376/PlEHn82RIwE1OVEtuvO1n317L0b0i91l2b8uwGs0mPE9-izVcqkJ49Uxd6rlV_30NzDq",
+        "parts": ["HAIR"]  # 監控這人的髮型
+    },
+    {
         "name": "흐접",
         "ppsn": "20372100000275176",
         "webhook_url": "https://discord.com/api/webhooks/1505922010264637522/h14VhSshRBlVL_mcCFNjTZHaG6yHR1kzwBOQZ9eS8jLn32lP83M-6xkKv3Wi87SZiWpk",
@@ -154,14 +166,14 @@ def monitor_loop():
                                     "embeds": [{
                                         "title": f"🚨 {name} 更換造型！",
                                         # 這裡把道具名稱加上 Markdown 語法，變成一鍵跳轉搜尋按鈕
-                                        "description": f"玩家穿上了 **{part_display}** 部位：**[{current_name}]({auto_search_url})**\n\n💡 *[點擊上方藍字道具名稱，即可自動開啟網站並搜尋該商品]*",
+                                        "description": f"玩家穿上了 **{part_display}**：**[{current_name}]**\n\n",
                                         "color": 3447003 if part == "HAIR" else 10181046, 
                                         "fields": [
                                             {"name": "🆔 商品 ID", "value": f"`{current_id}`", "inline": True},
-                                            {"name": "👤 創作者", "value": author, "inline": True}
+                                            {"name": "👤 作者", "value": author, "inline": True}
                                         ],
                                         "thumbnail": {"url": img_url},
-                                        "footer": {"text": f"造型獵手捕捉 • 部位: {part}"}
+                                        "footer": {"text": f"造型變更{part}"}
                                     }]
                                 }
                                 requests.post(webhook, json=payload, timeout=5)
